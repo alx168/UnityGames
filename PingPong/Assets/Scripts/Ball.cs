cfd3a4dc2782a3c0990de176d6cbe57fe313b9bb
atlema2 <atlema2@uic.edu>
2018-12-25 15:22:03 -0600

    added ball

---
+++ PingPong/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	private Rigidbody2D rg2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	rg2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+           	Vector2 vel = new Vector2(5f, 0f);
+        	rg2d.velocity = vel;
+
+
+        }
+    }
+}
---
+++ PingPong/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+
+ 
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+   	public GameObject spriteRenderer;
+   	public float restrictBack;
+   	public string[] moves = new string[4];
+   	private float restrictFront;
+ 	private float restrictBottom;
+ 	private float restrictTop;
+
+
+    private void Start() {
+    	SpriteRenderer board = gameObject.GetComponent<SpriteRenderer>();
+        if(board != null) {
+            restrictFront = 0f; // any vector2
+            restrictBottom = -4f;
+            restrictTop = 4f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moves[0]) && (this.transform.position.y < restrictTop)) // w
+        {
+        	Vector3 temp = new Vector3(0,.15f,0);  
+        	
+        	this.transform.position += temp;
+
+        }
+        if (Input.GetKey(moves[1]) && (this.transform.position.x > restrictBack)) // a
+        {
+            Vector3 temp = new Vector3(-.15f,0,0);  
+        	this.transform.position += temp;
+        }
+        if (Input.GetKey(moves[2]) && (this.transform.position.y > restrictBottom)) // s
+        {
+            Vector3 temp = new Vector3(0,-.15f,0);  
+        	this.transform.position += temp;
+        }
+        if (Input.GetKey(moves[3]) && (this.transform.position.x < restrictFront)) // d
+        {
+            Vector3 temp = new Vector3(.15f,0,0);  
+        	this.transform.position += temp;
+        }
+    }
+}
