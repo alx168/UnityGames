3a4e854d0341f05249b13c72bec3084f1d773e9c
atlema2 <atlema2@uic.edu>
2019-01-03 17:27:17 -0600

    Done With player anim

---
+++ 2DAdventure/GameIn2d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public Rigidbody2D theRB;
+	public float moveSpeed;
+	public bool canMove = true;
+
+	public Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if( canMove){
+        	theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * moveSpeed;
+        }
+
+        myAnim.SetFloat("MoveX", theRB.velocity.x);
+        myAnim.SetFloat("MoveY", theRB.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal")  == 1  ||  Input.GetAxisRaw("Horizontal")  == -1  || Input.GetAxisRaw("Vertical")  == 1  ||  Input.GetAxisRaw("Vertical")  == -1){
+        	myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+        	myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+        	
+        
+    }
+}
